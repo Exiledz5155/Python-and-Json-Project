9eb08b3f1c981364d59ffe7718b38f66e5d8ee48
Danny Bui <dannybui5155@gmail.com>
2023-04-25 15:06:51 -0500

    another commit word

--- main.py
+++ main.py
@@ -11,5 +11,19 @@ for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(index + 1, "-" , alternative)
-    user_choice = int(input("Enter your answer: "))
+    user_choice = int(input("Enter your answer: ").title())
     question["user_choice"] = user_choice
+    if question["user_choice"] == question["correct_answer"]:
+        score = score + 1
+
+for index, question in enumerate(data):
+    if question["user_choice"] == question["correct_answer"]:
+        score = score + 1
+        result = "Correct"
+    else:
+        result = "Incorrect"
+    message = f"{index + 1} - Your answer: {question['user_choice']}, "\
+            f"Correct answer: {question['correct_answer']}"
+
+
+print(score, "/", len(data))
