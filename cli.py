8b1e678c8cd06e63035f28311e4c467095e84812
Danny Bui <Dannybui5155@gmail.com>
2023-05-03 17:42:18 -0500

    Added to-dos for next commit

--- cli.py
+++ cli.py
@@ -18,7 +18,6 @@ params = {'limit': number_of_questions}
 
 response = requests.get('https://the-trivia-api.com/v2/questions', params=params)
 print(response.status_code)
-
 data = response.text
 json_data = json.loads(data)
 print(json_data)
@@ -61,15 +60,13 @@ for index, question in enumerate(json_data):
 print(f"Quiz complete! You got {user_correct} out of {number_of_questions} correct!")
 
 """
-create a list composed of the incorrect and correct answers. So we much access those first through the dict
-Randomize the list of incorrect and correct answers
-Save the correct answer in a variable 
-create user data list with each index being the number question and each object being correct or incorrect string
-Also track correct answers in each for loop iteration
-print out the answers 
-have the user choose a number
+Tiddy up code
 
+Implement difficulty feature
+    Use Match with a While loop
 
+Display additional data on user perfromance at the end
+    Give user option to see full questions, incorrect and correct answers (LOG)
 """
 
 
