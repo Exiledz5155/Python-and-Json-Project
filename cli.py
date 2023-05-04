c6a42b0ddd08e33b6815999a0c1a4f9c77600fa6
Danny Bui <Dannybui5155@gmail.com>
2023-05-03 20:29:01 -0500

    Cleaned up code and comments, implemented difficulty selection and colored terminal text

--- cli.py
+++ cli.py
@@ -1,53 +1,59 @@
 import json
 import requests
 import random
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
 
 
+colorama_init()
+user_data = []
+user_correct = 0
+
+# Ask the user for the amount of questions
 number_of_questions = int(input("Enter the number of questions. Minimum 1 and maximum 50: \n"))
 while not(0 < number_of_questions < 51):
     number_of_questions = int(input("Invalid number of questions. Try again: \n"))
 
-#difficulty = input("Enter a difficulty. Easy, medium or hard: \n")
-#difficulty = difficulty.lower()
+# Ask the user for the level of difficulty
+difficulty = input("Enter a difficulty. Easy, medium or hard: \n")
+difficulty = difficulty.lower()
+while difficulty not in ["easy", "medium", "hard"]:
+    difficulty = input("Invalid. Please try again: ")
 
-#while difficulty is not "easy" or
-
-#params = {'limit': number_of_questions, 'difficulties': difficulty}
-
-params = {'limit': number_of_questions}
-
+# Requesting and grabbing response from API
+params = {'limit': number_of_questions, 'difficulties': difficulty}
 response = requests.get('https://the-trivia-api.com/v2/questions', params=params)
-print(response.status_code)
+
+# Grabbing the text data and converting to JSON format
 data = response.text
 json_data = json.loads(data)
-print(json_data)
-print(type(json_data))
 
-user_data = []
-user_correct = 0
-
+# Iterate through each question and handle user input
 for index, question in enumerate(json_data):
 
+    # Printing the Question
     choices = []
-    print(f"Question {index + 1} of {number_of_questions}, Category: {question['category']} | {question['question']['text']}")
+    print(f"Question {index + 1} of {number_of_questions}, "
+          f"Category: {question['category'].replace('_', ' ').title()} "
+          f"| {Fore.GREEN}{question['question']['text']}{Style.RESET_ALL}")
 
     # Adding all choices to a list
     for index, choice in enumerate(question['incorrectAnswers']):
         choices.append(choice)
     choices.append(question['correctAnswer'])
 
-    #print(choices)
-
-    # Shuffling the choices (randomizing order displayed)
+    # Shuffling the choices (randomizing order displayed) and displaying choices
     random.shuffle(choices)
     for index, choice in enumerate(choices):
-        print(index + 1, choice)
-    #print("SHUFFLED", choices)
+        print(f"{Fore.LIGHTBLUE_EX}{index + 1}. {choice}{Style.RESET_ALL}")
 
     # Asking the user for input and handling the selected choice
     user_choice = int(input("Enter the number of your chosen answer: \n"))
     while not 0 < user_choice < 5:
         user_choice = int(input("That is an invalid choice, please try again: \n"))
+
+    # Tracking correct and incorrect answers from the user
     if choices[user_choice - 1] == question['correctAnswer']:
         # Question text, Correct Answer, Incorrect Answers, Boolean that represents User chose correctly or not, User choice
         user_data.append([question['question']['text'], question['correctAnswer'], question['incorrectAnswers'], True, choices[user_choice - 1]])
@@ -56,8 +62,8 @@ for index, question in enumerate(json_data):
         user_data.append([question['question']['text'], question['correctAnswer'], question['incorrectAnswers'], False, choices[user_choice - 1]])
 
 # Displaying results of the trivia quiz
+print(f"Quiz complete! You got {user_correct}/{number_of_questions} or {Fore.GREEN}{round(user_correct/number_of_questions*100, 2)}%{Style.RESET_ALL} correct!")
 
-print(f"Quiz complete! You got {user_correct} out of {number_of_questions} correct!")
 
 """
 Tiddy up code
@@ -65,7 +71,7 @@ Tiddy up code
 Implement difficulty feature
     Use Match with a While loop
 
-Display additional data on user perfromance at the end
+Display additional data on user performance at the end
     Give user option to see full questions, incorrect and correct answers (LOG)
 """
 
