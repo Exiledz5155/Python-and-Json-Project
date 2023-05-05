68667d005f0dddd226de735c8a75a011ddacad38
Danny Bui <Dannybui5155@gmail.com>
2023-05-05 17:22:34 -0500

    Implemented full results feature

--- cli.py
+++ cli.py
@@ -55,38 +55,58 @@ for index, question in enumerate(json_data):
 
     # Tracking correct and incorrect answers from the user
     if choices[user_choice - 1] == question['correctAnswer']:
-        # Question text, Correct Answer, Incorrect Answers, Boolean that represents User chose correctly or not, User choice
-        user_data.append([question['question']['text'], question['correctAnswer'], question['incorrectAnswers'], True, choices[user_choice - 1]])
+        # Question text, Correct Answer, All choices, Boolean that represents User chose correctly or not, User choice
+        user_data.append([question['question']['text'], question['correctAnswer'],
+                          choices, True, choices[user_choice - 1]])
         user_correct = user_correct + 1
     else:
-        user_data.append([question['question']['text'], question['correctAnswer'], question['incorrectAnswers'], False, choices[user_choice - 1]])
+        user_data.append([question['question']['text'], question['correctAnswer'],
+                          choices, False, choices[user_choice - 1]])
 
-# Displaying results of the trivia quiz
-print(f"Quiz complete! You got {user_correct}/{number_of_questions} or {Fore.GREEN}{round(user_correct/number_of_questions*100, 2)}%{Style.RESET_ALL} correct!")
+# Displaying basic results of the trivia quiz
+print(f"Quiz complete! You got {user_correct}/{number_of_questions} "
+      f"or {Fore.GREEN}{round(user_correct/number_of_questions*100, 2)}%{Style.RESET_ALL} correct!")
 
+# Prompting user to display the full results
+full_results = input(f"Would you like to see the full results? "
+                     f"Enter {Fore.GREEN}yes{Style.RESET_ALL} "
+                     f"or {Fore.RED}no{Style.RESET_ALL}: \n").lower()
+while full_results not in ["yes", "no"]:
+    difficulty = input("Invalid. Please try again: ").lower()
 
-"""
-Tiddy up code
+# Displaying the full results
+if full_results == "yes":
 
-Implement difficulty feature
-    Use Match with a While loop
+    # Iterate through each question
+    for index, question in enumerate(user_data):
 
-Display additional data on user performance at the end
-    Give user option to see full questions, incorrect and correct answers (LOG)
-"""
+        print(f"Question {index + 1} of {number_of_questions} | "
+              f"{Fore.GREEN}{question[0]}{Style.RESET_ALL}")
 
+        # User was correct
+        if question[3] == True:
+            for index, choice in enumerate(question[2]):
+                # Highlight correct chosen answer green
+                if (choice == question[1]):
+                    print(f"{Fore.GREEN}{index + 1} {choice} "
+                          f"<- You chose the correct answer{Style.RESET_ALL}")
+                else:
+                    print(f"{index + 1} {choice}")
+            print("")
 
+        # User was incorrect
+        if question[3] == False:
+            for index, choice in enumerate(question[2]):
+                # Highlight correct answer green
+                if (choice == question[1]):
+                    print(f"{Fore.GREEN}{index + 1} {choice} "
+                          f"<- Correct answer{Style.RESET_ALL}")
+                # Highlight incorrect chosen answer red
+                elif (choice == question[4]):
+                    print(f"{Fore.RED}{index + 1} {choice} "
+                          f"<- You chose this answer{Style.RESET_ALL}")
+                else:
+                    print(f"{index + 1} {choice}")
+            print("")
 
-"""
-with open('questions1.json', 'w') as file:
-    file.write("[")
-    for question in json_data:
-        file.write(str(question) + ",\n")
-    file.write("]")           
-"""
-
-"""
-with open('output.txt', 'w') as file:
-    for question in json_data:
-        file.write(str(question) + "\n")
-"""
+print("See ya next time!")
