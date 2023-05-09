4a6044e9fbf4030d3239b96c6c5d26e279565aa3
Danny Bui <Dannybui5155@gmail.com>
2023-05-08 20:43:24 -0500

    Unable to make this app work due to limitations by streamlit

--- API test.py
+++ API test.py
@@ -8,8 +8,42 @@ print(response.status_code)
 
 data = response.text
 json_data = json.loads(data)
-print(json_data)
+print(type(json_data))
 
-with open('output.txt', 'w') as file:
-    for question in json_data:
-        file.write(str(question) + "\n")
+"""while current_question < number_of_questions - 1:
+        question = quiz[current_question]
+        correct_answer = question["correctAnswer"]
+        options = [correct_answer] + question["incorrectAnswers"]
+        random.shuffle(options)
+
+        st.write(f"Question {current_question + 1} of {number_of_questions}, "
+          f"Category: {question['category'].replace('_', ' ').title()} "
+          f"| {question['question']['text']}")
+        user_answer = st.radio("Select your answer:", options)
+
+        while not st.button("Check Answer", key=f"check{current_question}"):
+            user_answers.append(user_answer)
+
+
+
+        if st.button("Next", key=f"next{current_question}") and current_question < number_of_questions - 1:
+            current_question = current_question + 1
+        elif current_question == number_of_questions - 1:
+            if st.button("Complete", key=f"complete{current_question}"):
+                break
+    """
+
+st.write(f"Quiz complete! You got {num_correct_answers}/{number_of_questions} "
+             f"({round(num_correct_answers/number_of_questions*100, 2)})")
+
+
+"""
+# Assign parameters and get a response from the API
+API_URL = "https://the-trivia-api.com/v2/questions"
+params = {'limit': number_of_questions, 'difficulties': difficulty}
+response = requests.get(API_URL, params=params)
+
+# Loading the response data to JSON format
+data = response.text
+quiz = json.loads(data)
+"""
--- webapp.py
+++ webapp.py
@@ -2,11 +2,47 @@ import streamlit as st
 import glob
 import pandas as pd
 import requests
+import random
+import json
 
 
-limit = 0
-difficulties = []
-API_URL = "https://the-trivia-api.com/v2/questions"
-params = {'limit': limit, 'difficulties': difficulties}
-response = requests.get(API_URL, params=params)
-print(response.status_code)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
+
+@st.cache_data
+def get_quiz_data(num_questions, difficulty):
+    API_URL = "https://the-trivia-api.com/v2/questions"
+    params = {'limit': num_questions, 'difficulties': difficulty}
+    response = requests.get(API_URL, params=params)
+    data = response.text
+    quiz = json.loads(data)
+    return quiz
+
+# Display title and selections for difficulty and # of Questions
+st.title("Trivia!")
+difficulty = st.selectbox("Select the difficulty", ['easy', 'medium', 'hard'])
+number_of_questions = st.slider("Select the number of questions", 1, 50)
+
+quiz = get_quiz_data(number_of_questions, difficulty)
+
+
+if st.button("Start Quiz", key="start"):
+    # current_question = 0
+    num_correct_answers = 0
+    user_answers = []
+
+    for current_question in range(number_of_questions):
+        question = quiz[current_question]
+        correct_answer = question["correctAnswer"]
+        options = [correct_answer] + question["incorrectAnswers"]
+        random.shuffle(options)
+
+        st.write(f"Question {current_question + 1} of {number_of_questions}, "
+                 f"Category: {question['category'].replace('_', ' ').title()} "
+                 f"| {question['question']['text']}")
+        user_answer = st.radio("Select your answer:", options)
+
+        if st.button("Check Answer", key=f"check{current_question}"):
+            if user_answer ==  correct_answer:
+                st.info("You're correct!")
+            else:
+                st.info("You're incorrect!")
